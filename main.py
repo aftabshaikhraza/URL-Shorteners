55bd63fb8ec8a4dcaf494738bda630a18d386dee
Aftab Shaikh <137868335+aftabshaikhraza@users.noreply.github.com>
2023-07-25 20:14:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pyshorteners
 
 long_url = input("Enter the URL to shorten: ")
 
-
-# TinyURL shortener service
-
 type_tiny = pyshorteners.Shortener()
 
 short_url = type_tiny.tinyurl.short(long_url)
